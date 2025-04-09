cffc88472a4ce9b846060ab16866df8f01ab6664
Kristoffer Troelsen <kristroelsen@hotmail.com>
2025-04-09 21:04:12 +0200

    xr rig ikke færdig

--- Assets/807/Scripts/HandController.cs
+++ Assets/807/Scripts/HandController.cs
@@ -4,47 +4,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandController : MonoBehaviour
 {
-    [Header("Input Action Reference")]
-    [SerializeField] private InputActionReference gripAction;
-
-    [Header("Animator")]
-    [SerializeField] private Animator handAnimator;
-
-    private void OnEnable()
+    public Animator handAnimator;         // Reference to the Animator
+    public InputAction gripAction;        // The grip action
+    private void Update()
     {
-        // Subscribe to the grip input actions
-        gripAction.action.performed += OnGripPerformed;
-        gripAction.action.canceled += OnGripCanceled;
+        // Get the grip value (should be between 0 and 1)
+        float gripValue = gripAction.ReadValue<float>();
 
-        // Enable the input action
-        gripAction.action.Enable();
-    }
-
-    private void OnDisable()
-    {
-        // Unsubscribe from the input actions
-        gripAction.action.performed -= OnGripPerformed;
-        gripAction.action.canceled -= OnGripCanceled;
-
-        // Disable the input action
-        gripAction.action.Disable();
-    }
-
-    private void OnGripPerformed(InputAction.CallbackContext context)
-    {
-        // Read the grip value as a float (this is the axis value)
-        float gripValue = context.ReadValue<float>();
-
-        // Optionally clamp the value to make sure it's within the expected range (0 to 1)
-        gripValue = Mathf.Clamp01(gripValue);
-
-        // Set the "Grip" parameter in the Animator
+        // Pass the grip value to the Animator to control the "Grip" parameter
         handAnimator.SetFloat("Grip", gripValue);
-    }
-
-    private void OnGripCanceled(InputAction.CallbackContext context)
-    {
-        // When grip is canceled, set the "Grip" parameter to 0 (Open Hand)
-        handAnimator.SetFloat("Grip", 0f);
     }
 }
