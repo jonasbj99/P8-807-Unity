36fb7137bf467edff84d83a9c1a0833dbddb1ea0
Kristoffer Troelsen <kristroelsen@hotmail.com>
2025-04-10 10:51:03 +0200

    xr rig setup færdig

--- Assets/807/Scripts/XRRigSetUp/HandController.cs
+++ Assets/807/Scripts/XRRigSetUp/HandController.cs
@@ -5,11 +5,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class HandController : MonoBehaviour
 {
     public Animator handAnimator;         // Reference to the Animator
-    public InputAction gripAction;        // The grip action
+    public InputActionProperty gripAction;        // The grip action
     private void Update()
     {
         // Get the grip value (should be between 0 and 1)
-        float gripValue = gripAction.ReadValue<float>();
+        float gripValue = gripAction.action.ReadValue<float>();
 
         // Pass the grip value to the Animator to control the "Grip" parameter
         handAnimator.SetFloat("Grip", gripValue);
