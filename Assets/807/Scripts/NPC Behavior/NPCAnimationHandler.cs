291ef0b4483f9214ca3af7f97045ee64f29a96f2
mandajohansen <aljo21@student.aau.dk>
2025-04-11 15:50:26 +0200

    NPC stuff

--- Assets/807/Scripts/NPC Behavior/NPCAnimationHandler.cs
+++ Assets/807/Scripts/NPC Behavior/NPCAnimationHandler.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class NPCAnimationHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Animator animator; // Reference to the Animator component
+    
+    // Animation parameter names
+    public string waveAnimation = "IsWaving";
+    
+
+    private void Awake()
     {
-        
+        // Get the Animator component attached to this GameObject
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayIdle()
     {
-        
+        if (animator != null)
+        {
+            // Set waving to false to return to idle
+            animator.SetBool(waveAnimation, false);
+        }
+        else
+        {
+            Debug.LogError("Animator component not found on this GameObject.");
+        }
+    }
+
+    public void PlayWave()
+    {
+        if (animator != null)
+        {
+            // Set waving to true to trigger wave animation
+            animator.SetBool(waveAnimation, true);
+        }
+        else
+        {
+            Debug.LogError("Animator component not found on this GameObject.");
+        }
     }
 }
