8a0ef69d4a562f899166d07019f439ac6a73e0c8
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2025-04-11 12:10:29 +0200

    SoundMeterUpdate

--- Assets/807/Scripts/UI/SoundMeterController.cs
+++ Assets/807/Scripts/UI/SoundMeterController.cs
@@ -6,48 +6,54 @@ public class SoundMeterController : MonoBehaviour
 {
     [SerializeField] TMP_Text soundLevelText;
 
-    [SerializeField] int sampleSize = 1024;
-    [SerializeField] float dBRange = 80f; // Decibel range 0 - dBRange
+    [SerializeField] int sampleSize = 1024; // Higher samples might provide smaller jitters at the cost of higher processing (Should be a power of two)
+    [SerializeField] float dBRange = 80f;   // Decibel range 0 - dBRange
+    [SerializeField] float interval = 0.5f;
     private float[] samples;
     private float rmsValue = 0f;
-    private float dbFS = 0f;         // dB relative to full scale (negative)
-    private float dbPositive = 0f;   // shifted to positive
+    private float dbFS = 0f;    
+    private float dbPositive = 0f;
 
-    void Start()
+    void OnEnable()
     {
         samples = new float[sampleSize];
+        StartCoroutine(SoundLevelReader());
     }
 
-    void Update()
+    void OnDisable()
     {
-        AudioListener.GetOutputData(samples, 0);
-
-        float sum = 0f;
-        for (int i = 0; i < sampleSize; i++)
-        {
-            sum += samples[i] * samples[i];
-        }
-
-        rmsValue = Mathf.Sqrt(sum / sampleSize);
-        dbFS = 20f * Mathf.Log10(rmsValue);
-
-        if (float.IsNegativeInfinity(dbFS))
-        {
-            dbFS = dBRange;
-        }
-
-        dbPositive = dbFS + dBRange;
-
-        Debug.Log(dbPositive);
-        soundLevelText.text = dbPositive.ToString("F1") + " dB";
+        StopAllCoroutines();
     }
 
-    /*
-    IEnumerator SoundLevelReader() 
+    IEnumerator SoundLevelReader()
     {
+        while (enabled)
+        {
+            AudioListener.GetOutputData(samples, 0);
 
+            float sum = 0f;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                sum += samples[i] * samples[i];
+            }
 
-        yield return new WaitForSeconds(0.5f);
+            rmsValue = Mathf.Sqrt(sum / sampleSize);
+            dbFS = 20f * Mathf.Log10(rmsValue);
+
+            if (float.IsNegativeInfinity(dbFS))
+            {
+                dbFS = -dBRange;
+            }
+
+            dbPositive = dbFS + dBRange;
+
+            Debug.Log(dbPositive);
+            if (soundLevelText != null)
+            {
+                soundLevelText.text = dbPositive.ToString("F1") + " dB";
+            }
+
+            yield return new WaitForSeconds(interval);
+        }
     }
-    */
 }
