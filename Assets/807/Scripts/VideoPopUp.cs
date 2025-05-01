29d87cc42aa05484460a2e9919de821bc18533ee
soiejn <julsgaard.sofie@gmail.com>
2025-05-01 12:40:24 +0200

    er I forelsket i mig ja eller nej 😻

--- Assets/807/Scripts/VideoPopUp.cs
+++ Assets/807/Scripts/VideoPopUp.cs
@@ -1,25 +1,58 @@
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class VideoEndPopup : MonoBehaviour
+public class VideoPopup : MonoBehaviour
 {
+    [Header("Video & Popup")]
     public VideoPlayer videoPlayer;
-    public SceneIntroduction sceneIntroduction; // Reference to your popup logic script
+    public SceneIntroduction sceneIntroduction;
+
+    [Header("XR Locomotion Locking")]
+    public ActionBasedContinuousMoveProvider moveProvider;
+    public ActionBasedContinuousTurnProvider turnProvider;
+    public UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation.TeleportationProvider teleportationProvider;
 
     void Start()
     {
         if (videoPlayer != null)
         {
+            videoPlayer.started += OnVideoStarted;
             videoPlayer.loopPointReached += OnVideoEnd;
         }
+
+        // Lock movement at start, assuming video plays immediately
+        LockMovement();
+    }
+
+    void OnVideoStarted(VideoPlayer vp)
+    {
+        LockMovement();
     }
 
     void OnVideoEnd(VideoPlayer vp)
     {
+        UnlockMovement();
+
         if (sceneIntroduction != null)
         {
-            sceneIntroduction.ShowPopup(); // Call the VR-aware popup logic
+            sceneIntroduction.ShowPopup();
         }
     }
+
+    void LockMovement()
+    {
+        if (moveProvider) moveProvider.enabled = false;
+        if (turnProvider) turnProvider.enabled = false;
+        if (teleportationProvider) teleportationProvider.enabled = false;
+    }
+
+    void UnlockMovement()
+    {
+        if (moveProvider) moveProvider.enabled = true;
+        if (turnProvider) turnProvider.enabled = true;
+        if (teleportationProvider) teleportationProvider.enabled = true;
+    }
 }
+
 
